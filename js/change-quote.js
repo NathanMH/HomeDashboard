f1ae8ff8adbf5de5c7886000881d71c7421479bc
Nathan Mador-House <nathan@musicnate.ca>
2016-06-19 18:59:55 -0400

    Refactor to reuse quote function.

--- js/change-quote.js
+++ js/change-quote.js
@@ -1,14 +1,5 @@
 function replace_quote() {
 
-//    var xhttp = new XMLHttpRequest();
-//    xhttp.onreadystatechange = function() {
-//	if (xhttp.readyState == 4 && xhttp.status == 200) {
-//	    document.getElementById("daily_quote").innerHTML = xhttp.responseText;
-//	}
-//    };
-//    xhttp.open("GET", "test_quotes.txt", true);
-//    xhttp.send();
-
     var request = new XMLHttpRequest();
     request.onload = function() {
 	var fileContent = this.responseText;
@@ -25,6 +16,27 @@ function replace_quote() {
 	document.getElementById("daily_quote_author").innerHTML = author;
     };
     request.open("GET", "assets/quotes-list.txt", true);
+    request.send();
+}
+
+function replace_pun() {
+
+    var request = new XMLHttpRequest();
+    request.onload = function() {
+	var fileContent = this.responseText;
+	var fileContentLines = fileContent.split( '\n' );
+	var punIndex = get_even_index(fileContentLines.length - 1);
+	var randomQuote = fileContentLines[punIndex];
+	var authorIndex = punIndex + 1;
+	var author = fileContentLines[authorIndex];
+//	console.log(fileContentLines.length);
+//	console.log(quoteIndex);
+//	console.log(authorIndex);
+	
+	document.getElementById("daily_quote_pun").innerHTML = randomQuote;
+	document.getElementById("daily_quote_pun_author").innerHTML = author;
+    };
+    request.open("GET", "assets/puns-list.txt", true);
     request.send();
 }
 
--- js/puns-and-quotes.js
+++
@@ -1,9 +0,0 @@
-function add_pun() {
-    var t = document.createTextNode("New Pun goes here!")
-    document.body.appendChild(t)
-}
-
-/* $.get('/assets/puns-list.txt', fuction() {
-    for i in 
-}, 'text');
-*/
