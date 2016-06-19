4704d1b6ef4f1cd7b3653a1d61516c3d33216ead
Nathan Mador-House <nathan@musicnate.ca>
2016-06-19 17:29:57 -0400

    Try javascript for dynamically adding items to home server pages.

---
+++ js/puns_and_quotes.js
@@ -0,0 +1,9 @@
+function add_pun() {
+    var t = document.createTextNode("New Pun goes here!")
+    document.body.appendChild(t)
+}
+
+/* $.get('/assets/puns-list.txt', fuction() {
+    for i in 
+}, 'text');
+*/
