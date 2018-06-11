2086ccf55ceba19235cbfe3a5c20e7495178ddf7
NathanMH <nathan@musicnate.ca>
2018-06-11 17:03:18 -0400

    start using php templates

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="navbar fixed-bottom ">
+	<p>&copy; 2018 Nathan Mador-House</p>
+</div>
---
+++ navbar.php
@@ -0,0 +1,35 @@
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		<a class="navbar-brand" href="/">L&N</a>
+		<ul class="navbar-nav">
+			<li class="nav-item dropdown">
+				<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+					Menu
+					<span class="caret"></span>
+				</a>
+				<div class="dropdown-menu">
+					<a class="dropdown-item" href="food.html">Food</a>
+					<a class="dropdown-item" href="tea.html">Tea List</a>
+					<a class="dropdown-item" href="alcohol.html">Alcohol</a>
+				</div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="http://192.168.0.1">Router</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="http://192.168.0.16/admin/">Pi-Hole</a>
+			</li>
+			<li class="nav-item dropdown">
+				<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+					WebDev Previews
+					<span class="caret"></span>
+				</a>
+				<div class="nav-item dropdown-menu" role="menu">
+					<a class="dropdown-item" href="http://192.168.1.100:81">MusicNate</a>
+					<a class="dropdown-item" href="http://192.168.1.100:82">ProntoMusica</a>
+				</div>
+			</li>
+		</ul>
+		</div>
+	</nav>
