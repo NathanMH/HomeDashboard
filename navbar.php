e9ae13a48179e69c0649805c79551a18d833a06c
NathanMH <nathan@musicnate.ca>
2018-07-12 16:18:42 -0400

    testing updated navbar for mobile

--- navbar.php
+++ navbar.php
@@ -1,36 +1,33 @@
-	<!-- Navbar -->
-	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-		<a class="navbar-brand" href="/">L&N</a>
-		<ul class="navbar-nav">
-			<li class="nav-item dropdown">
-				<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-					Menu
-					<span class="caret"></span>
-				</a>
-				<div class="dropdown-menu">
-					<a class="dropdown-item" href="food.php">Food</a>
-					<a class="dropdown-item" href="tea.php">Tea List</a>
-					<a class="dropdown-item" href="alcohol.php">Alcohol</a>
-					<a class="dropdown-item" href="games.php">Games</a>
-				</div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="http://192.168.0.1">Router</a>
-			</li>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">L&N</a>
+    </div>
 
-			<li class="nav-item">
-				<a class="nav-link" href="http://192.168.0.16/admin/">Pi-Hole</a>
-			</li>
-			<li class="nav-item dropdown">
-				<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-					WebDev Previews
-					<span class="caret"></span>
-				</a>
-				<div class="nav-item dropdown-menu" role="menu">
-					<a class="dropdown-item" href="http://192.168.1.100:81">MusicNate</a>
-					<a class="dropdown-item" href="http://192.168.1.100:82">ProntoMusica</a>
-				</div>
-			</li>
-		</ul>
-		</div>
-	</nav>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+			<li><a class="dropdown-item" href="food.php">Food</a></li>
+			<li><a class="dropdown-item" href="tea.php">Tea List</a></li>
+			<li><a class="dropdown-item" href="alcohol.php">Alcohol</a></li>
+			<li><a class="dropdown-item" href="games.php">Games</a></li>
+		  </ul>
+
+        <li><a href="http://192.168.0.1">Router</a></li>
+        <li><a href="http://192.168.0.16/admin/">Pi-Hole</a></li>
+
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
