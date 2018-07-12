5b9472240b09520bbca08d840aba64344fe33dc0
NathanMH <nathan@musicnate.ca>
2018-07-12 16:56:58 -0400

    Undo dark navbar....

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">L&N</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#chez-nav" aria-controls="chez-nav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
