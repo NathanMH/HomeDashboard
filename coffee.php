0a5d2642112c2d9e5b80e567cf2a476066ced75c
NathanMH <nathan@musicnate.ca>
2018-08-06 18:06:35 -0400

    Add to coffee page.

--- coffee.php
+++ coffee.php
@@ -14,28 +14,6 @@
 
 	<div class="container">
 
-			<button class="list-group-item filterDiv mixed" type="button" data-toggle="collapse" data-target="#negroni">
-				Negroni
-			</button>
-			<div class="collapse" id="negroni">
-				<div class="card card-body">
-					<div class="row">
-						<ul class="col">
-							<li>Gin</li>
-							<li>Campari</li>
-							<li>Red Vermouth</li>
-							<li>Orange Garnish</li>
-							<li>Ice</li>
-						</ul>
-						<ol class="col">
-							<li>Fill rocks glass with ice</li>
-							<li>Shake Gin (30ml), Vermouth (30ml), and Campari (30ml)</li>
-							<li>Strain into glass and garnish</li>
-						</ol>
-					</div>
-				</div>
-			</div>
-
 			<button class="list-group-item" type="button" data-toggle="collapse" data-target="#aeropress">
 				AeroPress
 			</button>
@@ -43,6 +21,7 @@
 				<div class="card card-body">
 					<div class="row">
 						<ul class="col">
+							<p>Regular</p>
 							<li>Heat the water: Bring your water to a boil then let it cool for about 1 minute. (You're aiming for something in between 175°F and 195°F.)</li>
 							<li>Grind the beans: Measure out 12g - 20g of ground coffee (about 4 tablespoons).</li>
 							<li>Wet the filter: Assemble the AeroPress with a paper filter inside the cap and place on top of a mug or cup. Drizzle a little warm water in to wet the filter.</li>
@@ -51,15 +30,40 @@
 							<li>Stir: Use the paddle stirrer (or a spoon) to stir once, briefly.</li>
 							<li>Press: Insert the plunger. Firmly press down the plunger until you hear a long hiss.</li>
 							<li>Taste and dilute: Taste the coffee and if desired add more water. The AeroPress makes a concentrated cup of coffee and if desired you can split this amount between two cups and add a little more hot water.</li>
+							<li>Enjoy!</li>
 						</ul>
 						<ol class="col">
-							<li></li>
-							<li></li>
-							<li></li>
+							<p>Inverted</p>
+							<li>Assemble the AeroPress upside-down: Heat the water and grind the coffee as described above. Assemble the AeroPress by putting plunger in the chamber. Flip upside-down but don't put the cap or filter on.</li>
+							<li>Pour in the coffee: Put the funnel in. Pour the ground coffee (from 2 AeroPress scoops of beans) into the the chamber.</li>
+							<li>Pour in the water: Add hot water until the chamber is almost full.</li>
+							<li>Stir</li>
+							<li>Steep for 1 minute: Try steeping for 1 minute, but adjust in future batches if this comes out too strong or too weak.</li>
+							<li>Put on the cap: Place a paper filter in the cap and screw tightly onto the chamber.</li>
+							<li>Flip over, carefully! Quickly and carefully flip the full AeroPress over so the cap is down, and place on a mug or pitcher.</li>
+							<li>Press</li>
+							<li>Enjoy!</li>
 						</ol>
 					</div>
 				</div>
 
+			</div>
+
+			<button class="list-group-item" type="button" data-toggle="collapse" data-target="#frenchpress">
+				AeroPress
+			</button>
+			<div class="collapse" id="frenchpress">
+				<div class="card card-body">
+					<div class="row">
+						<ul>
+							<li>Use 25g - 60g ground coffee (15-20g per cup water).</li>
+							<li>Boil water, but let it cool slightly.</li>
+							<li>Add water and stir.</li>
+							<li>Wait for a bit, then plunge.</li>
+							<li>Enjoy, but with friends!!</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 	</div>
 </body>
